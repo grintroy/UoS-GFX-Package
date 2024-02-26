7935bcbab898d99c1ea422e511686b3e4b4fd042
grintroy <grintroy@gmail.com>
2024-02-26 18:44:44 +0000

    Make the l3rd template

--- assets/js/l3rd.js
+++ assets/js/l3rd.js
@@ -1,38 +1,42 @@
-// Set initial data for webcg-devtools
 window.debugData = {
-  f0: "Lower third with a subtitle",
-  f1: "and CSS animations",
+	f0: "Name Surname",
+	f1: "Subtitle"
 };
 
 webcg.on("data", function (data) {
-  document.querySelector(".l3rd-1").innerHTML = data.f0
-    ? data.f0.text || data.f0
-    : "";
-  document.querySelector(".l3rd-2").innerHTML = data.f1
-    ? data.f1.text || data.f1
-    : "";
+	document.querySelectorAll(".name-stroke").forEach((el) => {
+		el.textContent = data.f0 ? data.f0.text || data.f0 : "Name Surname";
+	});
 });
 
 webcg.on("play", function () {
-  const overlay = document.querySelector(".overlay");
-  // Swap intro and outro classes
-  overlay.classList.add("intro");
-  overlay.classList.remove("outro");
-  // Return promise that is resolved when the animation is over. WebCG will wait for this promise.
-  return new Promise((resolve) => {
-    // Add the animationend event listener once
-    overlay.addEventListener("animationend", resolve, { once: true });
-  });
+	const lottieIntro = document.getElementById("lottie-intro");
+	lottieIntro.style.opacity = 1;
+	const lottieOutro = document.getElementById("lottie-outro");
+	lottieOutro.style.opacity = 0;
+	lottieIntro.play();
+	lottieOutro.stop();
+
+	setTimeout(() => {
+		const overlay = document.querySelector(".overlay");
+		overlay.style.opacity = 1;
+		const svg = overlay.querySelector("svg");
+		svg.style.top = "0px";
+	}, 300);
 });
 
 webcg.on("stop", function () {
-  const overlay = document.querySelector(".overlay");
-  // Swap intro and outro classes
-  overlay.classList.remove("intro");
-  overlay.classList.add("outro");
-  // Return promise that is resolved when the animation is over. WebCG will wait for this promise.
-  return new Promise((resolve) => {
-    // Add the animationend event listener once
-    overlay.addEventListener("animationend", resolve, { once: true });
-  });
+	const lottieOutro = document.getElementById("lottie-outro");
+	lottieOutro.style.opacity = 1;
+	const lottieIntro = document.getElementById("lottie-intro");
+	lottieIntro.style.opacity = 0;
+	lottieOutro.play();
+	lottieIntro.stop();
+
+	setTimeout(() => {
+		const overlay = document.querySelector(".overlay");
+		overlay.style.opacity = 0;
+		const svg = overlay.querySelector("svg");
+		svg.style.top = "50px";
+	}, 500);
 });
