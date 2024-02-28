fa27ea56589239b9d4c9ce69d92b8af5a5a4bc26
grintroy <grintroy@gmail.com>
2024-02-28 14:13:03 +0000

    Fix the CORS error while trying to load the lottie files for local environments

--- assets/js/l3rd.js
+++ assets/js/l3rd.js
@@ -1,42 +1,46 @@
+let lottieIntro, lottieOutro, overlay, svg;
+
 window.debugData = {
 	f0: "Name Surname",
 	f1: "Subtitle"
 };
 
-webcg.on("data", function (data) {
+webcg.on("data", (data) => {
 	document.querySelectorAll(".name-stroke").forEach((el) => {
 		el.textContent = data.f0 ? data.f0.text || data.f0 : "Name Surname";
 	});
 });
 
-webcg.on("play", function () {
-	const lottieIntro = document.getElementById("lottie-intro");
+webcg.on("play", () => {
 	lottieIntro.style.opacity = 1;
-	const lottieOutro = document.getElementById("lottie-outro");
 	lottieOutro.style.opacity = 0;
 	lottieIntro.play();
 	lottieOutro.stop();
 
 	setTimeout(() => {
-		const overlay = document.querySelector(".overlay");
 		overlay.style.opacity = 1;
-		const svg = overlay.querySelector("svg");
 		svg.style.top = "0px";
 	}, 300);
 });
 
-webcg.on("stop", function () {
-	const lottieOutro = document.getElementById("lottie-outro");
+webcg.on("stop", () => {
 	lottieOutro.style.opacity = 1;
-	const lottieIntro = document.getElementById("lottie-intro");
 	lottieIntro.style.opacity = 0;
 	lottieOutro.play();
 	lottieIntro.stop();
 
 	setTimeout(() => {
-		const overlay = document.querySelector(".overlay");
 		overlay.style.opacity = 0;
-		const svg = overlay.querySelector("svg");
 		svg.style.top = "50px";
 	}, 500);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+	lottieIntro = document.getElementById("lottie-intro");
+	lottieOutro = document.getElementById("lottie-outro");
+	overlay = document.querySelector(".overlay");
+	svg = overlay.querySelector("svg");
+
+	lottieIntro.load(lottie_intro_json);
+	lottieOutro.load(lottie_outro_json);
 });
